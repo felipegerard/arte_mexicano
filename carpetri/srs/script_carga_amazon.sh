3dd1be0b2a1eef008d64e4da34b3685d51cbff56
Carlos Petricioli <carpetri@gmail.com>
2015-05-28 12:16:11 -0500

    va un catalogo con tamaño de archivo para estadísticas

--- carpetri/srs/script_carga_amazon.sh
+++ carpetri/srs/script_carga_amazon.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo time
-head -4 ../catalogo_arte.txt
-
-ls /media/ironman/My\ Passport/digitalizacion/ | head -8  | parallel  's3cmd -r sync /media/ironman/My\ Passport/digitalizacion/{} s3://einformativa/biblioteca_arte/'
+date
+ls /media/ironman/My\ Passport/digitalizacion/ | head -80  | parallel  's3cmd -r sync /media/ironman/My\ Passport/digitalizacion/{} s3://einformativa/biblioteca_arte/'
+date
