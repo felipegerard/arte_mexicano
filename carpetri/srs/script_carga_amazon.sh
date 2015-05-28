f264a75bf6993aac13c8ea2a82a9331a23e3b4f7
Carlos Petricioli <carpetri@gmail.com>
2015-05-28 11:03:44 -0500

    van parriba los  primeros folders en parallelo para probar

--- carpetri/srs/script_carga_amazon.sh
+++ carpetri/srs/script_carga_amazon.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-time head -4 ../catalogo_arte.txt | parallel 's3cmd -r sync /media/ironman/My Passport/digitalizacion/{} s3://einformativa/biblioteca_arte/'
+echo time
+head -4 ../catalogo_arte.txt
+
+ls /media/ironman/My\ Passport/digitalizacion/ | head -8  | parallel  's3cmd -r sync /media/ironman/My\ Passport/digitalizacion/{} s3://einformativa/biblioteca_arte/'
