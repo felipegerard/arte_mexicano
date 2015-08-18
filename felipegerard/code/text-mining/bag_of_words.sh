619877ac31b2f5f9cfc149fc70e79a6f2b487f55
FelipeGerard <felipegerard@gmail.com>
2015-08-18 13:17:37 -0500

    Lista info para bag of words. Falta ponerles ayuda y documentarlas

--- felipegerard/code/text-mining/bag_of_words.sh
+++ felipegerard/code/text-mining/bag_of_words.sh
@@ -10,12 +10,19 @@ do
 	-t|--to)
 	TO="$2"
 	shift;;
+	-p|--parallel_args)
+	PARALLEL="$2"
+	shift;;
+	--mac)
+	SED_FLAG="-E"
+	SYSTEM="Mac OSX"
+	shift;;
 	*)
 	shift;;
     esac
 done
 
-txt2psv(){
+function txt2psv() {
     # args: file output
     < $1 tr '[:upper:]' '[:lower:]' \
 	| sed -E -e 's/[^0-9a-záéíóúüñ ]/ /g' -e 's/ +/ /g' \
@@ -26,13 +33,26 @@ txt2psv(){
 	| sed -E 's/ +([^ ]+) ([^ ]+)/\1|\2/' \
 	> $2
 }
+export -f txt2psv
 
-#txt2psv $FROM $TO
+if [[ ! "$SED_FLAG" ]]
+then
+    SED_FLAG="-r"
+    SYSTEM="Linux"
+fi
 
 if [ -d "${FROM}" ]
 then
     echo DIR
+    find $FROM \
+	| sed 1d \
+	| parallel $PARALLEL txt2psv {} $TO/{/.}.psv
 else
     echo FILE
     txt2psv "${FROM}" "${TO}"
 fi
+
+
+
+
+
---
+++ felipegerard/code/text-mining/join_bow.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+while [[ $# > 0 ]]
+do
+    key="$1"
+    case $key in
+	-f|--from)
+	FROM="$2"
+	shift;;
+	-t|--to)
+	TO="$2"
+	shift;;
+	-p|--parallel_args)
+	PARALLEL="$2"
+	shift;;
+	--force)
+	FORCE=true
+	shift;;
+	-n|--dry-run)
+	DRYRUN=true
+	shift;;
+	*)
+	shift;;
+    esac
+done
+
+if [ -z "$FROM" ] || [ -z "$TO" ]
+then
+    echo 'ERROR: Please provide a source directory and a target file.'
+else
+    if [ -f $TO ] && [ ! $FORCE ]
+    then
+	echo "ERROR: The file '$TO' already exists. Use option [--force] to override."
+    else
+	echo "Source directory: $FROM"
+	echo "Target file:      $TO"
+	if [ ! "$DRYRUN" ]
+	then
+	    find $FROM \
+		| sed 1d \
+		| parallel --keep-order $PARALLEL "< {} awk -F'|' '{print \"{/.}\" \"|\" \$2 \"|\" \$1}'" \
+	    > $TO
+	fi
+    fi
+fi
