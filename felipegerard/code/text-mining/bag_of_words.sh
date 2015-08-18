e99c4a5d528acb3a3989809dffe2454e4ec885a2
FelipeGerard <felipegerard@gmail.com>
2015-08-18 10:55:35 -0500

    Avances bag of words

--- felipegerard/code/text-mining/bag_of_words.sh
+++ felipegerard/code/text-mining/bag_of_words.sh
@@ -4,23 +4,35 @@ while [[ $# > 0 ]]
 do
     key="$1"
     case $key in
-	-f|--file)
-	FILE="$2"
+	-f|--from)
+	FROM="$2"
 	shift;;
-	-o|--output)
-	OUTPUT="$2"
+	-t|--to)
+	TO="$2"
 	shift;;
 	*)
 	shift;;
     esac
 done
 
-< $FILE tr '[:upper:]' '[:lower:]' \
-    | sed -E -e 's/[^0-9a-záéíóúüñ ]/ /g' -e 's/ +/ /g' \
-    | tr ' ' '\n' \
-    | grep -E '.' \
-    | sort \
-    | uniq -c \
-    | sed -E 's/ +([^ ]+) ([^ ]+)/\1|\2/' \
-    > $OUTPUT
+txt2psv(){
+    # args: file output
+    < $1 tr '[:upper:]' '[:lower:]' \
+	| sed -E -e 's/[^0-9a-záéíóúüñ ]/ /g' -e 's/ +/ /g' \
+	| tr ' ' '\n' \
+	| grep -E '.' \
+	| sort \
+	| uniq -c \
+	| sed -E 's/ +([^ ]+) ([^ ]+)/\1|\2/' \
+	> $2
+}
 
+#txt2psv $FROM $TO
+
+if [ -d "${FROM}" ]
+then
+    echo DIR
+else
+    echo FILE
+    txt2psv "${FROM}" "${TO}"
+fi
