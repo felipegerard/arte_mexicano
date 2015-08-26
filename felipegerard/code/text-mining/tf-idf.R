774bbdee1157b42ecdfcb0f39b1cffb2fac8df37
FelipeGerard <felipegerard@gmail.com>
2015-08-26 15:05:16 -0500

    Mejoras al codigo

--- felipegerard/code/text-mining/tf-idf.R
+++ felipegerard/code/text-mining/tf-idf.R
@@ -14,11 +14,10 @@ library(textcat)
 
 # Uso de memoria
 lh <- function(l){
-  require(pryr)
   options(scipen=999)
   data.frame(
     object = l,
-    mb = round((sapply(l, function(x) object_size(eval(parse(text = x)))))/2^20, 2)
+    mb = round((sapply(l, function(x) pryr::object_size(eval(parse(text = x)))))/2^20, 2)
   )
 }
 
@@ -28,6 +27,7 @@ clean_corpus <- function(corp,
                          remove_stopwords = TRUE,
                          null_text = 'TEXTOBASURA',
                          mc.cores = 4,
+                         lang = NULL,
                          allowed_languages = c('spanish','english',
                                                'french','german',
                                                'portugese')){
@@ -41,8 +41,8 @@ clean_corpus <- function(corp,
                    x <- tolower(x)
                    x <- gsub('[^ a-záéíóúüñ0-9]', '', x)
                    x <- gsub(' +', ' ', x)
-                   lang <- textcat(x)
-                   cat(lang)
+                   if(is.null(lang)) lang <- textcat(x)
+                   #cat(lang)
                    lang_cond <- !is.na(lang) && (lang %in% allowed_languages)
                    if(lang_cond && remove_stopwords){
                      x <- removeWords(x, stopwords(lang))
@@ -68,6 +68,93 @@ clean_corpus <- function(corp,
   #Corpus(VectorSource(corp))
 }
 
+# Calculo de scores
+analyze <- function(query, # String con el texto
+                    matriz, # TDM convertida a matriz
+                    dict, # Diccionario obtenido de la TDM
+                    meta, # Matriz con los metadatos del corpus
+                    min_wordlength = 2,
+                    lang = NULL)
+{
+  #limpieza del query
+  query_clean <- Corpus(VectorSource(query))
+  query_clean <- clean_corpus(query_clean,
+                              stem = FALSE,
+                              remove_stopwords = TRUE,
+                              lang = lang)
+  query_vec_1 <- TermDocumentMatrix(query_clean, 
+                                    control = list(dictionary = dict,
+                                                   wordLengths=c(min_wordlength, Inf))) 
+  norm_1 <- sqrt(sum(query_vec_1^2))
+  if(norm_1 == 0){
+    query_vec_2 <- query_vec_1 %>% as.matrix # Puros ceros
+  } else {
+    query_vec_2 <- as.matrix(query_vec_1)/norm_1 #normalizar con ntc el query
+  }
+  scores <- as.numeric(t(matriz)%*%query_vec_2)
+  
+  out <- data.frame(id=docnames,
+                    score=scores) %>%
+    filter(score != 0) %>%
+    left_join(meta) %>%
+    arrange(desc(score))
+  list(info = out,
+       query = query,
+       query_clean = query_clean,
+       query_vec = query_vec_2)
+}
+
+# Mostrar resultados
+print_results <- function(out, corp, mat, idx)
+{
+  info <- out$info[idx,]
+  cat(c('\n------------------------',
+        'Datos generales',
+        '------------------------',
+        paste('Libro:  ', as.character(info$origin)),
+        paste('Archivo:', as.character(info$id)),
+        paste('Idioma: ', as.character(info$lang)),
+        paste('Score:  ', round(info$score, 3))),
+      sep = '\n')
+  ver <- out$info$id[idx] %>% as.character
+  # La página tal cual
+  cat(c('\n------------------------',
+        'Texto original',
+        '------------------------\n'),
+      sep = '\n')
+  content <- tm_filter(corp, FUN = function(x) x$meta$id == ver)[[1]]$content %>%
+    gsub(pattern = '\\f', replacement = '\\\\f')
+  cat(content, sep = '\n')
+  # El query como realmente se usó después de procesarlo
+  cat(c('\n------------------------',
+        'Palabras clave',
+        '------------------------\n'),
+      sep = '\n')
+  q <- out$query_clean[[1]]$content
+  q <- sort(strsplit(q, ' ')[[1]])
+  cat(paste(q, collapse=', '))
+  cat('\n')
+  # Las líneas que hicieron match
+  cat(c('\n------------------------',
+        'Líneas que coincidieron',
+        '------------------------\n'),
+      sep = '\n')
+  query_grep <- out$query_clean[[1]]$content %>%
+    gsub(pattern = '^ ', replacement = '') %>%
+    gsub(pattern = ' ', replacement = '|')
+  cat(grep(query_grep, content, value = T, ignore.case = T), sep = '\n')
+  # Las palabras relevantes para el match
+  cat(c('\n------------------------',
+        'Plabras relevantes',
+        '------------------------\n'),
+      sep = '\n')
+  relevant <- mat[,ver] * out$query_vec
+  notzero <- (relevant > 0)
+  relevant <- data.frame(word = rownames(relevant)[notzero],
+                         score_contrib = as.numeric(relevant[,1])[notzero])
+  print(relevant)
+}
+
 # Control -----------------------------------------------------------------
 
 min_wordlength <- 2
@@ -83,19 +170,21 @@ allowed_languages <- c('spanish','english',
 # dir <- DirSource(directory = 'code/text-mining/test-by-page',
 #                  pattern = '[0-9]{5}.txt',
 #                  recursive = T)
-pages <- list.files('code/text-mining/test-by-page', #'code/pdftotext/txt/', #
-                    pattern = '[0-9]{5}.txt',
-                    full.names = T,
-                    recursive = T) %>%
-  grep(pattern = '/full/', invert = T, value = T)
-
-dir <- URISource(pages)
-corp_1 <- VCorpus(dir, readerControl = list(reader=readPlain))
-# Agregamos los nombres de los libros a los metadatos
-# Asumimos que la estructura es <ruta a libros>/libro/txt/archivo.txt
-for(i in 1:length(corp_1)){
-  corp_1[[i]]$meta$origin <- gsub('.*/([^/]+)/txt/[^/]+', '\\1', pages[i])
-}
+system.time({
+  pages <- list.files('code/text-mining/test-by-page', #'code/pdftotext/txt/', #
+                      pattern = '[0-9]{5}.txt',
+                      full.names = T,
+                      recursive = T) %>%
+    grep(pattern = '/full/', invert = T, value = T)
+  
+  dir <- URISource(pages)
+  corp_1 <- VCorpus(dir, readerControl = list(reader=readPlain))
+  # Agregamos los nombres de los libros a los metadatos
+  # Asumimos que la estructura es <ruta a libros>/libro/txt/archivo.txt
+  for(i in 1:length(corp_1)){
+    corp_1[[i]]$meta$origin <- gsub('.*/([^/]+)/txt/[^/]+', '\\1', pages[i])
+  }
+})
 system.time({
   corp_clean <- clean_corpus(corp_1,
                              mc.cores = 6,
@@ -104,30 +193,34 @@ system.time({
                              allowed_languages = allowed_languages)
 })
 
-# Para los primeros 75 libros con 6 procesos se tarda como 110 segs con stemming y 89 sin stemming
+# Para los primeros 75 libros (~ 100 MB) con 6 procesos se tarda como 110 segs con stemming y 89 sin stemming
+
+# Para los primeros 321 libros (~ 600 MB) con 6 procesos se tarda 427 segs en cargar los datos, 1058 segs en limpiarlos y 124 segs en generar la matriz de metadatos, la TDM, etc.
 
 corp_clean
 corp_clean[[2]]$meta
 corp_clean[[2]]$content
 
-docnames <- sapply(corp_clean, function(x) meta(x)$id)
-books <- sapply(corp_clean, function(x) meta(x)$origin)
-# languages <- mclapply(mc.cores = 6,
-#                       corp_clean,
-#                       function(x) textcat(x$content)) %>%
-#   unlist
-languages <- sapply(corp_clean, function(x) meta(x)$language)
-meta <- data.frame(origin=books,
-                   id=docnames,
-                   lang=languages) # No detecta tan bien... Poner moda por libro?
-
-tdm_1 <- TermDocumentMatrix(corp_clean,
-                            control=list(wordLengths = c(min_wordlength, Inf),
-                                         weighting = function(x)
-                                           weightSMART(x, spec='ntc')))
-dictionary_1 <- tdm_1$dimnames$Terms
-mat_1 <- sparseMatrix(i=tdm_1$i, j=tdm_1$j, x=tdm_1$v, dimnames = tdm_1$dimnames)
-tdm_1$dimnames$Docs # Igual a docnames: identical(tdm_1$dimnames$Docs, y = docnames)
+system.time({
+  docnames <- sapply(corp_clean, function(x) meta(x)$id)
+  books <- sapply(corp_clean, function(x) meta(x)$origin)
+  # languages <- mclapply(mc.cores = 6,
+  #                       corp_clean,
+  #                       function(x) textcat(x$content)) %>%
+  #   unlist
+  languages <- sapply(corp_clean, function(x) meta(x)$language)
+  meta <- data.frame(origin=books,
+                     id=docnames,
+                     lang=languages) # No detecta tan bien... Poner moda por libro?
+  
+  tdm_1 <- TermDocumentMatrix(corp_clean,
+                              control=list(wordLengths = c(min_wordlength, Inf),
+                                           weighting = function(x)
+                                             weightSMART(x, spec='ntc')))
+  dictionary_1 <- tdm_1$dimnames$Terms
+  mat_1 <- sparseMatrix(i=tdm_1$i, j=tdm_1$j, x=tdm_1$v, dimnames = tdm_1$dimnames)
+})
+#tdm_1$dimnames$Docs # Igual a docnames: identical(tdm_1$dimnames$Docs, y = docnames)
 
 
 # Ejemplo -----------------------------------------------------------------
@@ -137,48 +230,35 @@ tdm_1$dimnames$Docs # Igual a docnames: identical(tdm_1$dimnames$Docs, y = docna
 #query <- corp_1[[999]]$content %>% paste(collapse=' ')
 #query <- 'Hidalgo'
 #query <- 'primitive paintings'
-query <- 'american art XX century'
-query_lang <- textcat(query)
+#query <- 'México y las pinturas de Diego Rivera y Frida Kahlo'
+#query <- 'pintura en Uruapan Michoacán' # Muchas imágenes
+query <- 'arte colonial en Perú'
 
-#limpieza del query
-query_clean <- Corpus(VectorSource(query))
-query_clean <- clean_corpus(query_clean,
-                            stem = FALSE,
-                            remove_stopwords = TRUE)
-query_vec_1 <- TermDocumentMatrix(query_clean, 
-                                  control = list(dictionary = dictionary_1,
-                                                 wordLengths=c(min_wordlength, Inf))) 
-norm_1 <- sqrt(sum(query_vec_1^2))
-if(norm_1 == 0){
-  query_vec_2 <- query_vec_1 %>% as.matrix # Puros ceros
-} else {
-  query_vec_2 <- as.matrix(query_vec_1)/norm_1 #normalizar con ntc el query
-}
-scores <- as.numeric(t(mat_1)%*%query_vec_2)
+system.time(out <- analyze(query, mat_1, dictionary_1, meta, min_wordlength, lang = 'spanish'))
+head(out$info, 10)
+print_results(out, corp_1, mat_1, 1)
 
-out <- data.frame(id=docnames,
-                  score=scores) %>%
-  filter(score != 0) %>%
-  left_join(meta) %>%
-  arrange(desc(score))
-  #arrange(desc(lang == query_lang), lang, desc(score)) ### Hay que pensar esto mejor
+### HAY QUE EXCLUIR/MARCAR LAS PÁGINAS QUE CONTIENEN CASI PURAS IMÁGENES (CON AYUDA DE JARED), QUE SON ÍNDICES, ETC
+### 
 
-# Resultados
-out %>% head(10)
-ver <- out$id[1] %>% as.character
-content <- tm_filter(corp_1, FUN = function(x) x$meta$id == ver)[[1]]$content
-# La página tal cual
-content
-# El query como realmente se usó después de procesarlo
-query_clean[[1]]$content
-# Las líneas que hicieron match
-query_grep <- query_clean[[1]]$content %>%
-  gsub(pattern = '^ ', replacement = '') %>%
-  gsub(pattern = ' ', replacement = '|')
-grep(query_grep, content, value = T, ignore.case = T)
-# Las palabras relevantes para el match
-relevant <- mat_1[,ver] * query_vec_2
-relevant[relevant > 0,]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
