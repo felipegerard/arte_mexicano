4927644b9d8830b957bf37d077da30a529579983
jared275 <rocajare@gmail.com>
2015-09-18 10:57:59 -0500

    Muestreo libros pruebas

---
+++ jared/colectar_muestra_libros.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#Hago una seleccion aleatoria de 50 libros
+aws s3 ls einformativa/biblioteca_arte/ | sort --random-sort | head -100 | awk '{ print $2 }' > libros_muestra.txt
+
+cd Libros
+
+#Para cada uno de los libros hago una carpeta y voy guardando la informacion
+while read libro
+	do
+		mkdir $libro
+		aws s3 cp s3://einformativa/biblioteca_arte/$libro $libro --recursive
+		
+done < /home/jared/CONACyT_ITAM/libros_muestra.txt
