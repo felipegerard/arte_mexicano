80aaf74a305021ce9f449ddd2eafcdc4c346ae57
FelipeGerard <felipegerard@gmail.com>
2015-09-22 13:41:19 -0500

    Modificada deteccion de imagenes a que sea por libro

--- felipegerard/code/luigi/jared/data_flow_jared_modif.py
+++ felipegerard/code/luigi/jared/data_flow_jared_modif.py
@@ -1,3 +1,4 @@
+# coding=utf8
 import luigi
 import os
 import pickle
@@ -7,6 +8,14 @@ import multiprocessing
 import csv
 from collections import defaultdict
 from pprint import pprint
+
+def rgb2gray(rgb):
+			"""
+			Convertir imagenes en escala de grises
+			"""
+			r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
+			gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+			return gray
 
 class CarpetaLibro(luigi.ExternalTask):
 	"""
@@ -30,72 +39,37 @@ class IdentificarImagenes(luigi.Task):
 	Se a toma el listado de los jpgs y se analiza
 	si en esta pagina puede ver una pintura
 	"""
+	book_name = luigi.Parameter()
 	jpg_dir = luigi.Parameter() # Carpeta de jpgs (jpg/libro/pagina.jpg)
-	output_file = luigi.Parameter(default='imagenes.csv') # Archivo de salida con info de imagenes
+	output_dir = luigi.Parameter() # Archivo de salida con info de imagenes
 
 	def requires(self):
-		return [CarpetaLibro(os.path.join(self.jpg_dir, x)) for x in os.listdir(self.jpg_dir)] # Nos volamos el proceso intermedio
+		return CarpetaLibro(os.path.join(self.jpg_dir, self.book_name))
 
 	def output(self):		
-		return luigi.LocalTarget(self.output_file)
+		return luigi.LocalTarget(os.path.join(self.output_dir, self.book_name + '.images'))
 
 	def run(self):
-
-		# Nombres de todos los archivos
-		libros = [libro.path for libro in self.input()]
-		libros_jpgs = {}
-		for libro in libros:
+		if not os.path.exists(self.output_dir):
+			os.makedirs(self.output_dir)
+			print 'USER INFO: Creando carpeta de listas de archivos con imágenes en ' + self.output_dir
+		jpgs_lib = os.listdir(self.input().path)
+		resultado = []
+		for jpg in jpgs_lib:
+			ruta_jpg = os.path.join(self.input().path,jpg)
+			print ruta_jpg
 			try:
-				libros_jpgs[libro]= os.listdir(libro)
+				pagina = misc.imread(ruta_jpg)
+				pagina = rgb2gray(pagina)
+				if pagina.var()>5000:
+					resultado.append(jpg.replace('.jpg',''))
 			except:
-				pass
+				1
 
-		def rgb2gray(rgb):
-			"""
-			Convertir imagenes en escala de grises
-			"""
-			r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
-			gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
-			return gray
-
-		# Diccionario {libro:[paginas con imagenes]}
-		resultado = defaultdict(list) # Esto es para que por default los valores sean listas
-		# i = 0
-		for libro, jpgs_lib in libros_jpgs.iteritems():
-			pprint(dict(resultado)) # Para ir viendo el avance del objeto
-			# if i == 2:
-			# 	break
-			# else:
-			# 	i += 1
-			for jpg in jpgs_lib:
-				ruta_jpg = os.path.join(libro,jpg)
-				print ruta_jpg
-				try:
-					pagina = misc.imread(ruta_jpg)
-					pagina = rgb2gray(pagina)
-					if pagina.var()>5000:
-						resultado[os.path.split(libro)[-1]].append(jpg)
-				except:
-					1
-		"""Escribimos algo del tipo:
-		libro1
-		pag_con_imags_1.jpg
-		pag_con_imags_4.jpg
-
-		libro2
-		pag_con_imags_3.jpg
-		pag_con_imags_5.jpg
-		pag_con_imags_5.jpg
-		"""
-		resultado = '\n\n'.join([k + '\n' + '\n'.join(v) for k,v in resultado.iteritems()])
+		resultado = '\n'.join(resultado)
 		with self.output().open("w") as f:
 			f.write(resultado)
 
-		"""Para leer este resultado:
-		with open('test/models/imagenes.txt', 'r') as f:
-		    t = f.read()
-		res = {j[0]:j[1:] for j in [i.split('\n') for i in t.split('\n\n')] if len(j) > 1}
-		La ventaja es que también se puede abrir desde otros lados!"""
 
-if __name__ == '__main__':
-	luigi.run()
+# if __name__ == '__main__':
+	# luigi.run()
